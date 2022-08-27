6a8b3d8745f9489cdd168a577f23539c33f49c45
Arthur <me@shihao.li>
2022-08-28 02:15:42 +0800

    finished main loop

--- rei.go
+++ rei.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/signal"
+	"time"
 
 	"rei.io/rei/internal/helpers"
 	"rei.io/rei/internal/sui"
@@ -21,27 +23,101 @@ func initResume(name string) uint64 {
 		check(err)
 
 		// Read into cnt
-		_, err = fmt.Fscanf(file, "%d\n", &cnt)
+		_, err = fmt.Fscanf(file, "%d", &cnt)
 		check(err)
 		file.Close()
 	}
 	return cnt
 }
 
+func cleanUp(name string, cnt uint64) {
+
+	file, err := os.OpenFile(name, os.O_RDWR|os.O_CREATE, 0755)
+	check(err)
+
+	err = file.Truncate(0)
+	check(err)
+
+	_, err = file.Seek(0, 0)
+	check(err)
+
+	_, err = fmt.Fprintf(file, "%d", cnt)
+	check(err)
+
+	fmt.Println("Program killed !")
+	os.Exit(0)
+}
+
 func main() {
 
+	// File for count record
+	file := "count.conf"
+
 	// Get last stopped transaction count
-	cnt := initResume("count.conf")
+	cnt := initResume(file)
+	var max uint64
 
 	// Create new SUI client instance
-	sui := new(sui.SUIClient)
-	sui.Init("http://127.0.0.1:9000")
+	sc := new(sui.SUIClient)
+	sc.Init("http://127.0.0.1:9000")
 
-	// Get current total transactions count
-	max := sui.GetTotalTransactionNumber()
+	// Listener to kill
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, os.Interrupt)
 
-	_, _ = sui.GetTransactionsInRange(cnt, max)
+	// Mimic a do while loop... Always execute at least once
+	for {
+		max = sc.GetTotalTransactionNumber()
 
-	_, _ = sui.GetTransaction("Um5bXvoCztqZlhOy/xWslobwSTrXVxVt6QxDjYG+ep0=")
+		/*
+			Only triggered when max has been reached
+			Loop will run until max increases
+		*/
+		if max == cnt {
+			// This loop runs every 10 seconds
+			select {
+			case <-sigchan:
+				cleanUp(file, cnt)
 
+			// This basically allows us to wait for signal for 10 seconds
+			case <-time.After(10 * time.Second):
+			}
+
+		} else {
+
+			/*
+				Calculate how many transactions we need to retreve this iteration
+				If the diff is less than 4096, retrieve the diff
+				Otherwise retrieve 4096
+			*/
+			add := (func() uint64 {
+				if max-cnt > 4096 {
+					return 4096
+				} else {
+					return max - cnt
+				}
+			})
+
+			list, err := sc.GetTransactionsInRange(cnt, cnt+add())
+			check(err)
+
+			// We are listening for signal during *every transaction read*
+			for range list {
+
+				select {
+
+				// Graceful shutdown next transaction read
+				case <-sigchan:
+					cleanUp(file, cnt)
+
+				// If ctrl-c not triggered, process the transaction
+				default:
+
+					// count always before print so we don't skip transactions
+					cnt++
+					fmt.Printf("Finished processing %d\n", cnt)
+				}
+			}
+		}
+	}
 }
